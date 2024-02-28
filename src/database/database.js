04358b7571b2801be74ed67f8435a073f419387c
Joaquin Alvarez <joaquinbrialva@gmail.com>
2024-02-27 23:16:15 -0300

    Bug fix

--- src/database/database.js
+++ src/database/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { PORT, MONGO_URI } = process.env;
 
-mongoose.connect(MONGO_URI).then(() => console.log(`Corriendo en el puerto ${PORT}`))
+mongoose.connect(MONGO_URI)
+    .then(() => console.log(`Corriendo en el puerto ${PORT}`))
     .then(() => console.log('Conectado a Rapid Clothe Tracker'))
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
