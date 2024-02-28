48d45a6fbcfae53764f10b944b7bd0bac15ded00
Joaquin Alvarez <joaquinbrialva@gmail.com>
2024-02-27 23:11:11 -0300

    Se actualizo la URI

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const { PORT, MONGODB_STRING } = process.env;
+const { PORT, MONGO_URI } = process.env;
 
-mongoose.connect(MONGODB_STRING).then(() => console.log(`Corriendo en el puerto ${PORT}`))
+mongoose.connect(MONGO_URI).then(() => console.log(`Corriendo en el puerto ${PORT}`))
     .then(() => console.log('Conectado a Rapid Clothe Tracker'))
     .catch((error) => console.log(error))
