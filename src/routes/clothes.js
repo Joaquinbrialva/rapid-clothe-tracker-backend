0e148e2f7316fd3d4e763cb0378924a0aa523e07
Joaquin Alvarez <joaquinbrialva@gmail.com>
2024-03-01 01:31:18 -0300

    Se corrigio productId por clotheId

--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -5,6 +5,6 @@ router.get('/', getClothes);
 router.get('/:clotheId', getClothe);
 router.post('/', registerClothe);
 router.post('/sold/:clotheId', registerClotheSale);
-router.delete('/:productId', deleteClothe)
+router.delete('/:clotheId', deleteClothe)
 
 module.exports = router;
